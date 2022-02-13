58908a28e5f5f223f57a42bdef9d9db8c3f670a0
ArvinXY <arvinjunior@163.com>
2022-02-14 04:20:45 +0900

    feat(router): 初始化router
    初始化router

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
 
-  }
 ]
 
 const router = new VueRouter({
