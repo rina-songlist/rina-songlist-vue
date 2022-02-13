bd911e7fa1993942bb46dd9aea332aac8434dbe2
ArvinXY <arvinjunior@163.com>
2022-02-14 04:19:56 +0900

    feat(config): 添加了eslint相关配置
    添加了eslint相关配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,13 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0,
+    //空行最多不能超过3行
+    "no-multiple-empty-lines": [0, {"max": 3}],
+    "no-trailing-spaces": 'off',
+    //允许禁止混用tab和空格
+    "no-mixed-spaces-and-tabs": 'off'
+    // 'semi': [2, 'never']
   }
 }
