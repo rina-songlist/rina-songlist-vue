af149df19a16f85b23d6dffec81be6705ea6835f
ArvinXY <arvinjunior@163.com>
2022-02-14 03:55:04 +0900

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
