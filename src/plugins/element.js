ccdf5ec8494d9038e0ae1b0bb3f0600ddac416ef
ArvinXY <arvinjunior@163.com>
2022-02-17 06:35:03 +0900

    feat(element-ui): 添加了element-ui的按需导入
    添加了element-ui的按需导入

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,44 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {
+  Button,
+  Container,
+  Aside,
+  Header,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  Card,
+  Table,
+  TableColumn,
+  Pagination,
+  Message,
+  Row,
+  Col,
+  Input,
+  Loading,
+  Dialog,
+  Form,
+  FormItem
+} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Card)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Pagination)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Input)
+Vue.use(Loading)
+Vue.use(Dialog)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.prototype.$message = Message
