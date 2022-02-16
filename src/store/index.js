ad040c383f201c6d09b8a083d241e1285cd556c8
ArvinXY <arvinjunior@163.com>
2022-02-17 06:37:38 +0900

    feat(vuex): 添加了vuex配置
    添加了vuex配置

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 登陆状态 true：已登陆，false：未登陆
+    loginState: false,
+    token: '',
+    // 控制是否打开登陆窗口
+    loginDialogVisible: false
   },
   mutations: {
+    setLoginState(state, newState) {
+      state.loginState = newState
+    },
+    setToken(state, newToken) {
+      state.token = newToken
+    },
+    setLoginDialogVisible(state, newState) {
+      state.loginDialogVisible = newState
+    }
   },
   actions: {
   },
