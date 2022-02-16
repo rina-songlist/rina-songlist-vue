f793989299ab6cc5fc86ad1fb2eafe7e8fd4e6da
ArvinXY <arvinjunior@163.com>
2022-02-17 06:36:53 +0900

    feat(router): 添加了router配置
    添加了router配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import PublicSongList from '@/views/PublicSongList'
+
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/',
+    redirect: '/public'
+  },
+  {
+    path: '/public',
+    component: PublicSongList
+  }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/public') {
+    return next()
+  }
+
+  if (this.$store.state.token === '') {
+    return next('/public')
+  }
+  next()
 })
 
 export default router
