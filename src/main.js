adc8ebd3d081a82fa3ec5c87635c46165337eeb0
ArvinXY <arvinjunior@163.com>
2022-02-17 06:39:48 +0900

    feat(config): 更改了配置文件
    导入了vuex

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import './assets/css/global.css'
+
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
