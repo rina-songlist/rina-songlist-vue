a17040190b2cf9858f424766238bab499e380ab2
ArvinXY <arvinjunior@163.com>
2022-02-17 14:50:39 +0900

    fix(components): 修改了普通用户组件
    修改了密码未输入时提示内容错误的问题

--- src/views/PublicSongList.vue
+++ src/views/PublicSongList.vue
@@ -107,7 +107,7 @@ export default {
           { required: true, message: '请输入用户名', trigger: 'blur' }
         ],
         password: [
-          { required: true, message: '请输入用户名', trigger: 'blur' }
+          { required: true, message: '请输入密码', trigger: 'blur' }
         ]
       }
     }
