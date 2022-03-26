7a69e5a1c20ed7d4db5a54dbb38428091d8ff0dd
ArvinXY <arvinjunior@163.com>
2022-03-26 16:40:37 +0900

    feat(api): 添加了api接口
    添加了菜单的接口

---
+++ src/api/menu.js
@@ -0,0 +1,13 @@
+import axios from '@/utils/axiosUtilsWithToken'
+
+// 查看菜单列表
+export const listMenu = (reqParams) => { return axios.get('/private/system/menu', { params: reqParams }) }
+
+// 获取单一菜单
+export const getSingleMenu = (reqParams) => { return axios.get('/private/system/menu/' + reqParams) }
+
+// 添加（更改）一条菜单
+export const editMenu = (reqParams) => { return axios.put('/private/system/menu', reqParams) }
+
+// 删除一条菜单
+export const deleteMenu = (reqParams) => { return axios.delete('/private/system/menu/' + reqParams) }
