8b3e55b622bcb7d2320fb075d081ab7d9adae9d9
ArvinXY <arvinjunior@163.com>
2022-03-26 16:41:00 +0900

    feat(api): 添加了api接口
    添加了歌单的接口

--- src/api/songList.js
+++ src/api/songList.js
@@ -1,3 +1,17 @@
-import axios from '@/utils/axiosUtils'
+import axios from '@/utils/axiosUtilsWithToken'
+import axios2 from '@/utils/axiosUtilsWithoutToken'
 
-export const publicSongList = (reqParams) => { return axios.get('/song-list/public-list', { params: reqParams }) }
+// 普通用户所看到的歌单信息
+export const publicSongList = (reqParams) => { return axios2.get('/song-list', { params: reqParams }) }
+
+// 管理用户所看到的歌单信息
+export const listSongList = (reqParams) => { return axios.get('/private/system/song-list', { params: reqParams }) }
+
+// 获取单条歌单
+export const getSingleList = (reqParams) => { return axios.get('/private/system/song-list/' + reqParams) }
+
+// 添加（更改）一条歌单
+export const editSongList = (reqParams) => { return axios.put('/private/system/song-list', reqParams) }
+
+// 删除一条歌单
+export const deleteSongList = (reqParams) => { return axios.delete('/private/system/song-list/' + reqParams) }
