d2093eff5fb655de69e537a9723a05df726bf98e
ArvinXY <arvinjunior@163.com>
2022-03-28 03:56:45 +0900

    feat(utils): 更改了无token的axios配置
    添加了请求和返回体的拦截器

--- src/utils/axiosUtilsWithoutToken.js
+++ src/utils/axiosUtilsWithoutToken.js
@@ -1,4 +1,8 @@
 import axios from 'axios'
+import qs from 'qs'
+import Vue from 'vue'
+
+var vm = new Vue()
 
 switch (process.env.NODE_ENV) {
   case 'production':
@@ -16,6 +20,26 @@ switch (process.env.NODE_ENV) {
  * 设置是否允许跨域携带身份验证（即cookies）
  */
 axios.defaults.timeout = 10000
-// axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true
+
+axios.interceptors.request.use(config => {
+  // 将数组参数序列化
+  if (config.method === 'get') {
+    config.paramsSerializer = function(params) {
+      return qs.stringify(params, { arrayFormat: 'repeat' })
+    }
+  }
+  return config
+}, error => {
+  vm.$message.error('无法连接服务器！')
+  return Promise.reject(error)
+})
+
+axios.interceptors.response.use(response => {
+  return response
+}, error => {
+  vm.$message.error('无法连接服务器！')
+  return Promise.reject(error)
+})
 
 export default axios
