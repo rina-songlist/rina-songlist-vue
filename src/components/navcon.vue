bfd34a7e2657cf3ae7d5f0e481b7cefc001c386a
ArvinXY <arvinjunior@163.com>
2022-03-28 08:09:30 +0900

    feat(components): 更改了左侧栏的组件
    完善了顶部边栏的内容

--- src/components/navcon.vue
+++ src/components/navcon.vue
@@ -9,38 +9,90 @@
       <el-menu-item index="1" @click="openLoginMenu">
         <img src="@/assets/pictures/rina.png" class="rina-logo" alt="网页出错了请重新加载_(´ཀ`」 ∠)_"/>
       </el-menu-item>
-      <!--TODO 点击跳转至普通用户页-->
-      <el-menu-item index="2">林莉奈歌单</el-menu-item>
+      <el-menu-item index="2" @click="jumpToPublic">林莉奈歌单</el-menu-item>
       <el-menu-item index="3"><a href="https://space.bilibili.com/1243266187" target="_blank">b站主页</a></el-menu-item>
       <el-menu-item index="4"><a href="https://live.bilibili.com/22742508?spm_id_from=333.999.0.0" target="_blank">直播间</a></el-menu-item>
       <!--当前用户信息和状态（登陆后显示）-->
       <el-submenu index="5" v-if="loginState" class="submenu">
-        <template slot="title">{{ userName }}</template>
-        <!--TODO 这边后面要做退出登陆的实现方法-->
-        <el-menu-item index="5-1">退出登陆</el-menu-item>
+        <template slot="title">{{ currentUser }}</template>
+        <el-menu-item index="5-1" @click="logoutDialogVisible = true">退出登陆</el-menu-item>
       </el-submenu>
     </el-menu>
+
+    <!--退出登陆提示框-->
+    <el-dialog
+      title="提示"
+      :visible.sync="logoutDialogVisible"
+      width="30%">
+      <span>确定要退出登陆吗？</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="logoutDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="logout">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import { mapState, mapMutations } from 'vuex'
+import { getSingleMenu } from '@/api/menu'
 
 export default {
   data () {
     return {
-      userName: '1234'
+      // 退出登陆对话框的显示和隐藏
+      logoutDialogVisible: false
     }
   },
   computed: {
-    ...mapState(['loginState'])
+    ...mapState(['loginState', 'currentUser'])
   },
   created () {
+    if (this.currentUser === '') {
+      const currentUser = window.sessionStorage.getItem('user')
+      this.setCurrentUser(currentUser)
+    }
   },
   methods: {
-    ...mapMutations(['setLoginDialogVisible']),
+    ...mapMutations(['setLoginDialogVisible', 'setLoginState', 'setCurrentUser']),
+    // 打开登陆菜单
     openLoginMenu() {
-      this.setLoginDialogVisible(true)
+      getSingleMenu(1).then(res => {
+        if (res.data.code === 200) {
+          this.setLoginState(true)
+          this.setLoginDialogVisible(false)
+          if (this.$route.path !== '/show/song-list') {
+            this.$router.push('/show/song-list')
+          }
+        } else {
+          if (this.$route.path !== '/public') {
+            this.$router.push('/public')
+          }
+
+          this.setLoginState(false)
+          this.setLoginDialogVisible(true)
+        }
+      })
+    },
+    // 跳转回普通用户页面
+    jumpToPublic() {
+      if (this.$route.path !== 'public') {
+        this.$router.push('/public')
+      }
+    },
+    // 退出登陆
+    logout() {
+      // 登陆状态的监测数据全部初始化
+      this.setLoginState(false)
+      this.setCurrentUser('')
+      window.localStorage.setItem('token', undefined)
+
+      // 跳转回默认页面
+      this.logoutDialogVisible = false
+      this.$message.success('退出登陆成功！')
+      if (this.$route.path !== 'public') {
+        this.$router.push('/public')
+      }
     }
   }
 }
