ace1a5baec001b3672656cbde916144194d95f20
ArvinXY <arvinjunior@163.com>
2022-03-31 07:23:54 +0900

    feat(element-ui): 更改了element的配置
    添加了弹框组件的导入

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -25,7 +25,8 @@ import {
   Switch,
   Tooltip,
   Select,
-  Option
+  Option,
+  Tree
 } from 'element-ui'
 
 Vue.use(Button)
@@ -52,5 +53,6 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Select)
 Vue.use(Option)
+Vue.use(Tree)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
