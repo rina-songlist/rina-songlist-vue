a2caccad6ac4a3610cdf838824a299022b73a2c6
ArvinXY <arvinjunior@163.com>
2022-03-31 07:25:12 +0900

    feat(api): 更改了role的api
    添加了获取当前权限下的菜单和更改可查看菜单的接口

--- src/api/role.js
+++ src/api/role.js
@@ -3,11 +3,17 @@ import axios from '@/utils/axiosUtilsWithToken'
 // 获取权限列表
 export const listRole = () => { return axios.get('/private/system/role') }
 
+// 获取当前权限下的菜单
+export const listRoleMenus = (reqParams) => { return axios.get('/private/system/roleMenu/' + reqParams) }
+
 // 获取单条权限
 export const getSingleRole = (reqParams) => { return axios.get('/private/system/role/' + reqParams) }
 
 // 添加（更改）一条权限
 export const editRole = (reqParams) => { return axios.put('/private/system/role', reqParams) }
+
+// 更改可查看菜单
+export const changeMenus = (roleId, menuIds) => { return axios.post('/private/system/role/' + roleId, menuIds) }
 
 // 删除一条权限
 export const deleteRole = (reqParams) => { return axios.delete('/private/system/role/' + reqParams) }
