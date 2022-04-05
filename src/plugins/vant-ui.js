b4704f7b453b531117dd4a3d5b403b7b6205ae05
ArvinXY <arvinjunior@163.com>
2022-04-05 23:05:34 +0900

    feat(vant-ui): 添加了vant-ui
    添加并导入了vant-ui

---
+++ src/plugins/vant-ui.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import {
+  Icon,
+  NavBar,
+  Image as VanImage,
+  Search,
+  Cell,
+  CellGroup,
+  List,
+  Sticky
+} from 'vant'
+
+Vue.use(Icon)
+Vue.use(NavBar)
+Vue.use(VanImage)
+Vue.use(Search)
+Vue.use(Cell)
+Vue.use(CellGroup)
+Vue.use(List)
+Vue.use(Sticky)
