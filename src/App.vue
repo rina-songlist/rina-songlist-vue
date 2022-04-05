2022166df2861a269ff38c051e0cb93eb5fffeff
ArvinXY <arvinjunior@163.com>
2022-04-05 22:04:00 +0900

    feat(components): 更改了PC端的主入口的组件
    将PC端的框架移除，以便做多设备兼容

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,23 @@
 <template>
-  <el-container class="main-container">
-    <el-aside width="200px" v-if="loginState">
-      <leftnav></leftnav>
-    </el-aside>
-    <el-container>
-      <el-header>
-        <navcon></navcon>
-      </el-header>
-      <el-main>
-        <!--路由占位符-->
-        <router-view></router-view>
-      </el-main>
-    </el-container>
-  </el-container>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex'
-import leftnav from '@/components/leftnav'
-import navcon from '@/components/navcon'
 
 export default {
   name: 'app',
-  components: {
-    leftnav,
-    navcon
-  },
   data() {
-    return {
-
-    }
-  },
-  computed: {
-    ...mapState(['loginState'])
+    return {}
   },
   created () {
-    if (!this.loginState) {
-      const state = window.sessionStorage.getItem('state')
-      this.setLoginState(state)
-    }
   },
   methods: {
-    ...mapMutations(['setLoginState'])
-  },
-  beforeMount () {
-    // 在进入普通用户界面前将登陆状态改为未登陆
-    if (this.$route.path === '/public') {
-      this.setLoginState(false)
-    }
   }
 }
 </script>
 
 <style lang="less" scoped>
-.main-container {
-  height: 100%;
-}
-.el-header {
-  padding: 0;
-}
+
 </style>
