a12263795e8bdff16a81cf76cebeadf4623b33a4
ArvinXY <arvinjunior@163.com>
2022-04-08 09:23:08 +0900

    feat(config): 添加了生产环境下的main配置
    添加了生产环境下的main配置

---
+++ src/main-prod.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import '@/assets/css/global.css'
+import '@/assets/fonts/iconfont.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
