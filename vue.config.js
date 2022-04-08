8211c88c4666ba69a6d3c1dbfdaff7d25fdfe3d8
ArvinXY <arvinjunior@163.com>
2022-04-08 09:26:52 +0900

    feat(config): 添加了vue全局配置
    添加了vue全局配置

---
+++ vue.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  devServer: {
+    port: 8080,
+    host: '0.0.0.0',
+    https: false
+  },
+  chainWebpack: config => {
+    // 生产模式
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js')
+
+      config.set('externals', {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        vuex: 'Vuex',
+        axios: 'axios'
+      })
+
+      config.plugin('html').tap(args => {
+        args[0].isProd = true
+        return args
+      })
+    })
+
+    // 开发模式
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+
+      config.plugin('html').tap(args => {
+        args[0].isProd = false
+        return args
+      })
+    })
+  }
+}
